4d40fa1aed19ba457dcb5468249e6ead9915a938
shanmukhg44 <shanmukhg@digi9.co.in>
2025-03-10 10:02:08 +0530

    fix 2

--- src/pages/validationPage/ValidationPage.jsx
+++ src/pages/validationPage/ValidationPage.jsx
@@ -1,63 +1,87 @@
-import React, { useEffect } from 'react';
-import { useUser } from '@clerk/clerk-react';
-import { getUserByClerkId, getUserBySessionId } from '../../api/userApi';
-import { useLocation, useNavigate } from 'react-router-dom';
-import Validation from "../../assets/Validating.gif"
+import React, { useEffect } from "react";
+import { useUser } from "@clerk/clerk-react";
+import { getUserByClerkId, getUserBySessionId } from "../../api/userApi";
+import { useLocation, useNavigate } from "react-router-dom";
+import Validation from "../../assets/validating.gif";
 
 export default function ValidationPage() {
   const { isSignedIn, user, isLoaded, sessionId } = useUser();
   const navigate = useNavigate();
-  const location= useLocation();
+  const location = useLocation();
 
   useEffect(() => {
     // Only run the effect if the user is signed in and the user data has been loaded
     const apiCaller = async () => {
-      console.log("location",location);
-      console.log("isSignedIn", isSignedIn,"user",user,"isLoaded",isLoaded,"sessionId",sessionId);
+      console.log("location", location);
+      console.log(
+        "isSignedIn",
+        isSignedIn,
+        "user",
+        user,
+        "isLoaded",
+        isLoaded,
+        "sessionId",
+        sessionId
+      );
       // if (isSignedIn && isLoaded && user) {
       //   console.log("User:", user, "Session ID:", sessionId);
-        
-        try {
-          console.log("location",location.state);
-          const clerId= await getUserBySessionId({ sessionId: location.state.sessionId });
-          console.log(clerId);
-          const data = await getUserByClerkId(clerId.userId);
-          console.log("hehe",data.data);
-          localStorage.setItem("sessionId", JSON.stringify(location.state.sessionId));
-          if (data.data.user.user_role === "user") {
-            if (data.data.user.profile_status === true) {
-             
-              navigate("/user",{state: location.state.sessionId });
-            } else {
-              navigate("/personalInfo");
-            }
-          } else if (data.data.user.user_role === "admin") {
-            
-            navigate("/admin");
-          }
-          // console.log(data.data.user.user_role ,"mslksk");
 
-        } catch (error) {
-          console.error("Error fetching user data:", error);
+      try {
+        console.log("location", location.state);
+        const clerId = await getUserBySessionId({
+          sessionId: location.state.sessionId,
+        });
+        console.log(clerId);
+        const data = await getUserByClerkId(clerId.userId);
+        console.log("hehe", data.data);
+        localStorage.setItem(
+          "sessionId",
+          JSON.stringify(location.state.sessionId)
+        );
+        if (data.data.user.user_role === "user") {
+          if (data.data.user.profile_status === true) {
+            navigate("/user", { state: location.state.sessionId });
+          } else {
+            navigate("/personalInfo");
+          }
+        } else if (data.data.user.user_role === "admin") {
+          navigate("/admin");
         }
+        // console.log(data.data.user.user_role ,"mslksk");
+      } catch (error) {
+        console.error("Error fetching user data:", error);
       }
+    };
     // };
 
     // Only call the API if the user is signed in and loaded
     // if (isSignedIn && isLoaded) {
-      apiCaller();
+    apiCaller();
     // }
-
   }, []); // Add dependencies to re-run the effect when values change
 
   // Optional: Show a loading indicator while the user data is being loaded
   if (!isLoaded || !isSignedIn) {
-    return   <div style={{ display: "flex",flexDirection: "column", justifyContent: "center", alignItems: "center" , width:"100%",height:"100vh" }}>Validating...
-    <img src={Validation} alt='loading' style={{width:"200px",height:"200px" }} />
-    </div>;
+    return (
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          width: "100%",
+          height: "100vh",
+        }}
+      >
+        Validating...
+        <img
+          src={Validation}
+          alt="loading"
+          style={{ width: "200px", height: "200px" }}
+        />
+      </div>
+    );
   }
 
-  return (
-  <></>
-  );
+  return <></>;
 }
